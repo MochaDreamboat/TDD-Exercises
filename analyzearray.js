b72aca41787f23cf0327b35e2c928172b71952c3
mochadreamboat <jersotmac@gmail.com>
2022-05-10 21:20:33 -0500

    Created function that returns statistics of given array.

---
+++ analyzearray.js
@@ -0,0 +1,25 @@
+let analyzeArray = (arr) => {
+
+    const average = getAverage(arr);
+    const min = getMin(arr);
+    const max = getMax(arr);
+    const length = arr.length();
+
+    const getAverage = arr => {
+        let sum = arr.reduce((a,b) => a + b);
+        let length = arr.length()
+        return (sum / length)
+    };
+
+    const getMin = arr => {
+        let minimum = arr.reduce((a,b) => a < b ? a : b)
+        return minimum;
+    };
+
+    const getMax = arr => {
+        let maximum = arr.reduce((a,b) => a > b ? a : b)
+        return maximum;
+    };
+
+    return {average, min, max, length}
+};
